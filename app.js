fe8c4b28df699942c5c3f88be2a7e682910087c4
saracristinas <sarasales17062000@gmail.com>
2021-04-14 22:48:34 -0300

    apontando pro arquivo index

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // create an express app
 const express = require("express")
 const app = express()
+var path = require('path')
 
 // use the express-static middleware
 app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
-
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
